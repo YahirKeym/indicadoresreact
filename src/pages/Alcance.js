ddc147d4bd964e2f747f0df289e24f0961ea1597
Yahir Keym <arzkaner@gmail.com>
2019-07-08 17:43:40 -0500

    Se agrega la página de alcances

---
+++ src/pages/Alcance.js
@@ -0,0 +1,10 @@
+import React from 'react';
+class Alcance extends React.Component{
+    render()
+    {
+        return (
+            <h1>Hola alcances</h1>
+        );
+    }
+}
+export default Alcance;
