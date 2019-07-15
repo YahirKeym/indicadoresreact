ded2dabdbd5323316a85e3cc7c0f6d1dba7ac550
Yahir Keym <arzkaner@gmail.com>
2019-07-15 17:38:30 -0500

    Se retoca el side bar para hacerlo vertical

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -3,18 +3,16 @@ import {Link} from 'react-router-dom';
 function SideBar()
 {
     return (
-        <div className="col-8 ">
-            <ul>
-                <div>
+        <div className="col-12 d-flex justify-content-center row text-center">
+                <div className="col-2">
                     <Link className="navbar-brand" to="/tipos/alcance">Tipos de Alcance</Link>
                 </div>
-                <div>
+                <div className="col-2">
                     <Link className="navbar-brand" to="/paises">Paises</Link>
                 </div>
-                <div>
+                <div className="col-2">
                     <Link className="navbar-brand" to="/jerarquia">Jerarquia</Link>
                 </div>
-            </ul>
         </div>
     );
 }
