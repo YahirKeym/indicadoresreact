43a630671ac86707b53ec8929c8c21a80acb164a
Yahir Keym <arzkaner@gmail.com>
2019-07-18 13:11:43 -0500

    Será el cuerpo de los mandos y de los objetivos

---
+++ src/components/CuerpoObjetivosMandos.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import './styles/Mandos.css';
+/**
+ * Cambiara los acentos que vengan con codificación java
+ */
+function acentosEncodeJava(cadena = ""){
+    cadena = cadena.replace(/u00E1/gi,"á");
+    cadena = cadena.replace(/u00C1/gi,"Á");
+    cadena = cadena.replace(/u00E9/gi,"é");
+    cadena = cadena.replace(/u00C9/gi,"É");
+    cadena = cadena.replace(/u00ED/gi,"í");
+    cadena = cadena.replace(/u00CD/gi,"Í");
+    cadena = cadena.replace(/u00F3/gi,"ó");
+    cadena = cadena.replace(/u00D3/gi,"Ó");
+    cadena = cadena.replace(/u00D3/gi,"ú");
+    cadena = cadena.replace(/u00DA/gi,"Ú");
+    cadena = cadena.replace(/u00F1/gi,"ñ");
+    cadena = cadena.replace(/u00D1/gi,"Ñ");
+    return cadena;
+}
+/**
+ * Guarda el componente de descrición del Mando
+ * @param {propiedades} props 
+ */
+function Descripcion(props){
+    let Descripcion = acentosEncodeJava(props.descripcion);
+    if(Descripcion.length === 0){
+        Descripcion = "Este indicador no cuenta con una descripción, por favor de agregar una";
+    }
+    return(
+            <p>{Descripcion}</p>
+    )
+}
+/**
+ * Guarda el componente del titulo
+ * @param {propiedades} props 
+ */
+function Titulo(props)
+{
+    let MuestraTitulo = props.titulo;
+    if(MuestraTitulo.length === 0){
+        MuestraTitulo = props.objetivo
+    }
+    return(
+        <h4>{MuestraTitulo}</h4>
+    );
+}
+/**
+ * Es el componente que nos ayudara a tenter un cuerpo general
+ * @param {props} props 
+ */
+function CuerpoObjetivosMandos(props){
+    const titulo = props.titulo;
+    const subtitulo = props.subtitulo;
+    const descripcion = props.descripcion;
+    const url = props.url;
+    const textSuccess = props.textSuccess;
+    const urlDelete = props.delete;
+    return(
+        <div className="col-12 p-3 col-md-6 row mando mx-auto text-white">
+            <div  className="col-12 text-center">
+                <Titulo titulo={titulo} objetivo={subtitulo} />
+            </div>
+            <div className="col-12 p-2 mando-text">
+                <Descripcion descripcion={descripcion} />
+            </div>
+            {props.children}
+            <div className="col-12 mt-3">
+                <Link className="btn btn-success" to={url}>{textSuccess}</Link>
+                <Link className="btn btn-danger ml-3" to="">Eliminar</Link>
+            </div>
+        </div>
+    )
+}
+export default CuerpoObjetivosMandos;
