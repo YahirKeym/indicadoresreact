7cd56bd6e84b2978b693f86dd3276c3c0e75d172
Yahir Keym <arzkaner@gmail.com>
2019-07-18 13:12:12 -0500

    se comienzan a hacer algunos ajustes visuales a la sección de mandos

--- src/pages/Mandos.js
+++ src/pages/Mandos.js
@@ -1,29 +1,79 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import './styles/Mandos.css';
-function DescripcionMando(props){
-    let Descripcion = props.descripcion;
-    if(Descripcion.length === 0){
-        Descripcion = "Este indicador no cuenta con una descripción, por favor de agregar una";
-    }
+import CuerpoObjetivosMandos from '../components/CuerpoObjetivosMandos.js';
+
+/**
+ * Guardara los badges de Jerarquias
+ * @param {*} props 
+ */
+function JerarquiasMando(props)
+{
+    let jerarquias = props.jerarquias;
+    console.log(jerarquias)
+    let rangos = props.rangos;
     return(
-        <div className="col-12 p-2 mando-text">
-            <p>{Descripcion}</p>
-        </div>
+        <React.Fragment>
+            {jerarquias.datos.map(rangoMando=>{
+            return(
+                <React.Fragment key={rangoMando.id}>
+                    {rangos.map(rango => {
+                        let Element = <span key={rango.id}></span>;
+                        if(rangoMando.id === rango.id){
+                            Element = <span key={rango.id} className="badge badge-light mt-3 mr-3">{rango.nombre}</span>;
+                        }
+                        return(
+                            Element
+                        );
+                    })}
+                </React.Fragment>
+                )
+            })}
+        </React.Fragment>
     )
 }
-function TituloMando(props)
+/**
+ * Guardara el componente de las variables del mando
+ * @param {propiedades} props 
+ */
+function VariablesMando(props)
 {
-    let MuestraTitulo = props.titulo;
-    if(MuestraTitulo.length === 0){
-        MuestraTitulo = props.objetivo
-    }
+    const variables = props.variables;
     return(
-    <div  className="col-12 text-center">
-        <h4>{MuestraTitulo}</h4>
-    </div>
-    );
+        <React.Fragment>
+            {variables.map(variable =>{
+                return(
+                    <div className="col-12 row variable" key={variable.id}>
+                        <div className="col-12">
+                            {quitaEspeciales(variable.nombre)}
+                        </div>
+                        <div className="col-10 row">
+                            {variable.etapas.map(etapa =>{
+                                return(
+                                <div className="col-2 text-center etapa" key={etapa.id}>
+                                    {etapa.valor}
+                                </div>
+                                )
+                            })}
+                        </div>
+                        <div className="col-2">
+                            Total: {variable.valorTotal}
+                        </div>
+                    </div>
+                );
+            })}
+        </React.Fragment>
+    )
 }
+/**
+ * Quita caracteres especiales
+ */
+function quitaEspeciales(cadena = ""){
+    cadena = cadena.replace(/_/gi," ");
+    return cadena;
+}
+/**
+ * Será la clase de mandos donde se visualizaran todos
+ */
 class Mandos extends React.Component{
     constructor(props)
     {
@@ -37,19 +87,19 @@ class Mandos extends React.Component{
     {
         this.traerDatos();
     }
+    /**
+     * Tr
+     */
     traerDatos = async () =>{
         const req = await fetch(`${this.props.url}&action=view`);
         const response =  await req.json();
-        const reqObjetivos = await fetch(`${this.props.urlObjetivos}&action=view`);
-        const responseObjetivos = await reqObjetivos.json();
-        if(response.status && responseObjetivos.status)
+        if(response.status)
         {
             if(response.datos.length === 0){
                 response.datos = [];
             }
             this.setState({
-                data: response.datos,
-                objetivos: responseObjetivos.datos
+                data: response.datos
             })
         }
     } 
@@ -58,60 +108,28 @@ class Mandos extends React.Component{
         console.log(this.state)
         return (
             <React.Fragment>
-                <Link to="/mandos/add" className="btn btn-success">Añadir nuevo mando</Link>
                 <div className="col-12 mt-3 row">
-                    {this.state.data.map(mando => {
-                        return(
-                            <div className="col-12 p-3 col-md-6 row mando mx-auto text-white" key={mando.id}>
-                                <TituloMando titulo={mando.datos.titulo} objetivo={mando.objetivosData.titulo} />
-                                <DescripcionMando descripcion={mando.objetivosData.descripcion} />
-                                <div className="col-12 p-2 mando-control">
-                                {mando.variables.map(variable =>{
-                                    return(
-                                        <div className="col-12 row variable" key={variable.id}>
-                                            <div className="col-12">
-                                                {variable.nombre}
-                                            </div>
-                                            <div className="col-10 row">
-                                                {variable.etapas.map(etapa =>{
-                                                    return(
-                                                    <div className="col-2 text-center etapa" key={etapa.id}>
-                                                        {etapa.valor}
-                                                    </div>
-                                                    )
-                                                })}
-                                            </div>
-                                            <div className="col-2">
-                                                Total: {variable.valorTotal}
-                                            </div>
-                                        </div>
-                                    );
-                                })}
-                                </div>
-                                <div className="col-12">
-                                    {mando.datos.jerarquias.datos.map(rangoMando=>{
-                                        return(
-                                            <React.Fragment key={rangoMando.id}>
-                                                {mando.rangos.map(rango => {
-                                                    var Element = <span key={rango.id}></span>;
-                                                    if(rangoMando.id === rango.id){
-                                                       Element = <span key={rango.id} className="badge badge-light mt-3 mb-3 mr-3">{rango.nombre}</span>;
-                                                        }
-                                                        return(
-                                                            Element
-                                                            );
-                                                        })}
-                                            </React.Fragment>
-                                        )
-                                    })}
-                                </div>
-                                <div className="col-12">
-                                    <Link className="btn btn-success" to={`/mandos/${mando.id}`}>Ver</Link>
-                                    <button className="btn btn-danger ml-3">Eliminar</button>
-                                </div>
-                            </div>
-                        );
-                    })}
+                    <div className="col-5 mb-3 mx-auto">
+                        <Link to="/mandos/add" className="btn btn-success col-12">Añadir nuevo mando</Link>
+                    </div>
+                    <div className="col-12 row">
+                        {this.state.data.map(mando => {
+                            return(
+                                <CuerpoObjetivosMandos textSuccess="Ver" key={mando.id} 
+                                titulo={mando.datos.titulo} id={mando.id} 
+                                subtitulo={mando.objetivosData.titulo} url={`/mandos/${mando.id}`} 
+                                descripcion={mando.objetivosData.descripcion}
+                                delete={`/mandos/${mando.id}/delete`}>
+                                    <div className="col-12 p-2 mando-control">
+                                        <VariablesMando variables={mando.variables} />
+                                    </div>
+                                    <div className="col-12">
+                                        <JerarquiasMando jerarquias={mando.datos.jerarquias} rangos={mando.rangos}/>
+                                    </div>
+                                </CuerpoObjetivosMandos>
+                            );
+                        })}
+                    </div>
                 </div>
             </React.Fragment>
         );
