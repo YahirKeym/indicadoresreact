38bbeb64f6297601de6fbea3f03ee7dd2cb445fb
Yahir Keym <arzkaner@gmail.com>
2019-07-18 13:13:13 -0500

    se comienza a desarrollar el perfil del mando

--- src/pages/MandosProfile.js
+++ src/pages/MandosProfile.js
@@ -1,12 +1,88 @@
 import React from 'react';
+import CuerpoObjetivosMandos from '../components/CuerpoObjetivosMandos.js';
+/**
+ * Guardara el componente de las variables del mando
+ * @param {propiedades} props 
+ */
+function VariablesMando(props)
+{
+    const variables = props.variables;
+    return(
+        <React.Fragment>
+            {variables.map(variable =>{
+                return(
+                    <div className="col-12 row variable" key={variable.id}>
+                        <div className="col-12">
+                            {quitaEspeciales(variable.nombre)}
+                        </div>
+                        <div className="col-10 row">
+                            {variable.etapas.map(etapa =>{
+                                return(
+                                <div className="col-2 text-center etapa" key={etapa.id}>
+                                    {etapa.valor}
+                                </div>
+                                )
+                            })}
+                        </div>
+                        <div className="col-2">
+                            Total: {variable.valorTotal}
+                        </div>
+                    </div>
+                );
+            })}
+        </React.Fragment>
+    )
+}
+/**
+ * Quita caracteres especiales
+ */
+function quitaEspeciales(cadena = ""){
+    cadena = cadena.replace(/_/gi," ");
+    return cadena;
+}
 export default class MandosProfile extends React.Component{
     constructor(props)
     {
         super(props);
         this.state = {
-            datos: {
-                datos:{},
-                objetivos:[]
+            data:{
+                "variables":[],
+                 "datos":{  
+                    "formula":"",
+                    "etapas":0,
+                    "tipoDeEtapa":"",
+                    "titulo":"",
+                    "unidadDeMedida":"",
+                    "AceptacionBuena":"0",
+                    "AceptacionMedia":"0",
+                    "objetivo":"0",
+                    "rangos":{  
+                       "datos":[],
+                       "global":false
+                    },
+                    "jerarquias":{  
+                       "datos":[],
+                       "global":true
+                    },
+                    "usuarios":{  
+                       "datos":[]
+                    }
+                 },
+                 "objetivos":[],
+                 "objetivosData":{  
+                    "titulo":"",
+                    "descripcion":""
+                 },
+                 "rangos":[],
+                 "jeraraquias":[],
+                 "users":[],
+                 "errors":{  
+                    "formulaNoCoincideConVariables":true
+                 },
+                 "acciones":[  
+           
+                 ],
+                 "objetivoShow":true
             }
         }
     }
@@ -18,24 +94,30 @@ export default class MandosProfile extends React.Component{
      * Traera los datos de los objetivos y del mando.
      */
     traerDatos = async () =>{
-        const [mando,objetivos]= await Promise.all([
-            fetch(`${this.props.url}&action=select&id=${this.props.match.params.mandoId}`),
-            fetch(`${this.props.urlObjetivos}&action=view`)
-        ]);
+        const mando= await fetch(`${this.props.url}&action=select&id=${this.props.match.params.mandoId}`);
         const responseMando = await mando.json();
-        const responseObjetivos = await objetivos.json();
-        if(responseMando.status && responseObjetivos)
+        if(responseMando.status)
         {
             this.setState({
-                datos: responseMando.datos,
-                objetivos: responseObjetivos
+                data: responseMando.datos
             })
         }
     }
     render()
     {
         return (
-            <h1>PROFILE</h1>
+            <CuerpoObjetivosMandos url="" titulo={this.state.data.datos.titulo} descripcion={this.state.data.objetivosData.descripcion}>
+                <div className="col-12 mt-3">
+                    <button className="btn-light btn">Editar datos</button>
+                </div>
+                {console.log(this.state.data.variables)}
+                <div className="col-12 p-2 mando-control">
+                    <VariablesMando variables={this.state.data.variables} />
+                </div>
+                <div className="col-12">
+                    <h4>Aquí debería de ir una grafica</h4>
+                </div>
+            </CuerpoObjetivosMandos>
         );
     }
 }
