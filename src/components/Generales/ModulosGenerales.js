8203784c2c9f77c9869934a18c23e3a99a5615a4
Yahir Keym <arzkaner@gmail.com>
2019-07-30 11:22:16 -0500

    se crea el componente para tener los modulos generales en uno solo

---
+++ src/components/Generales/ModulosGenerales.js
@@ -0,0 +1,3 @@
+import CodificaMalos from '../../components/Generales/CodificaMalos';
+import DecodificaMalos from '../../components/Generales/DecodificaMalos';
+export {CodificaMalos,DecodificaMalos};
