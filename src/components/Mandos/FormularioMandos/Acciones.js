b2f4082aa86b3c08dd1e9518193a1feab0518adb
Yahir Keym <arzkaner@gmail.com>
2019-07-31 17:43:19 -0500

    Se depura un poco la sección de acciones y se agrega la nueva funcionalidad de añadir x elemento a x array

--- src/components/Mandos/FormularioMandos/Acciones.js
+++ src/components/Mandos/FormularioMandos/Acciones.js
@@ -1,29 +1,35 @@
 import React from 'react';
 import {Input} from '../../Formulario/ModulosFormulario';
 import DeleteOneElement from '../../Generales/DeleOneElement';
+import AddOneElement from '../../Generales/AddOneElement';
 function Acciones(props){
     const objeto = props.objeto,
     lugarDeDatos = props.lugarDeDatos;
     return(
-        <React.Fragment>
+        <div className="col-12 m-0">
+            <h4>Acciones a tomar</h4>
+            <div className="col-12 m-0 mt-3 p-0 row mb-3">
+                <div className="col-4 m-0 p-0">
+                    <button className=" btn btn-success" onClick={e => {
+                        AddOneElement(e,objeto,lugarDeDatos.acciones,"Acción",[
+                            {"nombre":"asda","valor":"lala"}
+                        ]);
+                    }}>Agregar Acción +</button>
+                </div>
+                {lugarDeDatos.acciones.length !== 0 && (
+                    <div className="col-4 m-0 p-0">
+                        <button className="btn btn-danger" onClick={e => DeleteOneElement(e,lugarDeDatos.acciones,0,objeto)}>Eliminar Acción -</button>
+                    </div>
+                )}
+            </div>
             {lugarDeDatos.acciones.map(accion => {
                 return(
-                    <div key={accion.id} className="col-8 mt-2">
+                    <div key={accion.id} className="col-8 m-0 mt-2 p-0">
                         <Input type="text" datos={{_self:objeto,lugar:lugarDeDatos.acciones[accion.id-1],zona:"nombre"}} defaultValue={accion.nombre} plhold={accion.nombre} />
                     </div>
                 )
             })}
-            <div className="col-12 mt-3">
-                <div className="col-6 m-0 p-0">
-                    <button className=" btn btn-success" onClick={objeto.handleAddAction}>Agregar Acción +</button>
-                </div>
-                {lugarDeDatos.acciones.length !== 0 && (
-                    <div className="col-6 m-0 p-0">
-                        <button className="btn btn-danger mt-3" onClick={e => DeleteOneElement(e,lugarDeDatos.acciones,0,objeto)}>Eliminar Acción -</button>
-                    </div>
-                )}
-            </div>
-        </React.Fragment>
+        </div>
     )
 }
 export default Acciones;
