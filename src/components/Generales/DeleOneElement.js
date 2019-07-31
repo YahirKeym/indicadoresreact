b7d54728d7e90f0da744b1028802cfab0a0f20b9
Yahir Keym <arzkaner@gmail.com>
2019-07-31 09:05:29 -0500

    Se eliminara una variable que no estaba siendo ocupada y se deja solo la función

--- src/components/Generales/DeleOneElement.js
+++ src/components/Generales/DeleOneElement.js
@@ -3,7 +3,7 @@ function DeleteOneElement(e,elementos,minimoDeElementos,_self){
     e.preventDefault();
     const cantidadDeElementos = elementos.length;
     if(cantidadDeElementos > minimoDeElementos){
-        const elementosEliminados = elementos.splice(cantidadDeElementos-1,1);
+        elementos.splice(cantidadDeElementos-1,1);
         _self.setState(_self.state)
     }
 }
