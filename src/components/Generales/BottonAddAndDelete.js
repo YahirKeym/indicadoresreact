3054ccc3b2614aeb9f530d424d2e8c3c6c9292f2
Yahir Keym <arzkaner@gmail.com>
2019-07-31 17:47:32 -0500

    Se crea el componente que contendra los botones add and delete element del dom el cual hace trabajo con AddOneElement y DeleteOneElement

---
+++ src/components/Generales/BottonAddAndDelete.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import AddOneElement from "./AddOneElement";
+import DeleteOneElement from "./DeleOneElement";
+
+function BottonAddandDelete(props){
+    const objeto = props.objeto,
+    lugarDeDatos = props.lugarDeDatos,
+    cantidadMinima = parseInt(props.minimo),
+    datosExtra = props.extra,
+    {nombre, nombreAgregar, nombreEliminar} = props.datos;
+    return(
+        <div className="col-12 m-0 mt-3 p-0 row mb-3">
+            <div className="col-4 m-0 p-0">
+                <button className=" btn btn-success" onClick={e => {
+                    AddOneElement(e,objeto,lugarDeDatos,nombre,datosExtra);
+                }}>
+                    {nombreAgregar}
+                </button>
+            </div>
+            {lugarDeDatos.length !== cantidadMinima && (
+                <div className="col-4 m-0 p-0">
+                    <button className="btn btn-danger" 
+                    onClick={e => DeleteOneElement(e,lugarDeDatos,cantidadMinima,objeto)}>
+                        {nombreEliminar}
+                    </button>
+                </div>
+            )}
+        </div>
+    )
+}
+export default BottonAddandDelete;
