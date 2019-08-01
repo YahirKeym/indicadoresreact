7f25d22d19b8bdac0ee3e1fe7480dfe5914f9ed4
Yahir Keym <arzkaner@gmail.com>
2019-08-01 18:10:12 -0500

    Se arreglan algunos bugs para agregar variables

--- src/components/Mandos/FormularioMandos/Variables.js
+++ src/components/Mandos/FormularioMandos/Variables.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Input} from '../../Formulario/ModulosFormulario';
 import DeleteOneElement from '../../Generales/DeleOneElement';
+import AddVariable from './AddVariable';
 // Se encargara de pintar las variables del indicador en modo formulario.
 function Variables(props){
     const objeto = props.objeto,
-    lugarDeDatos = props.lugarDeDatos;
+    lugarDeDatos = props.lugarDeDatos,
+    lugarDeDatosPrincipal = props.lugarDeDatosPrincipal;
     return(
         <div className="col-12 mt-3 row">
             <h4 className="col-12">Variables del indicador</h4>
@@ -16,10 +18,12 @@ function Variables(props){
                 </div>)
             })}
             <div className="col-12 col-lg-4 mt-2">
-                <button onClick={objeto.handleAddVariable} type="button" className="btn btn-success">+</button>
-                {lugarDeDatos.variables.length > 2 && (
+                <button onClick={e =>{
+                    AddVariable(e,objeto,lugarDeDatosPrincipal,lugarDeDatos.variables)
+                }} type="button" className="btn btn-success">+</button>
+                {lugarDeDatos.variables.length > 1 && (
                     <button onClick={e => {
-                        DeleteOneElement(e,lugarDeDatos.variables,2,objeto);
+                        DeleteOneElement(e,lugarDeDatos.variables,1,objeto);
                     }} type="button" className="btn btn-danger ml-2">-</button>
                 )}
             </div>
