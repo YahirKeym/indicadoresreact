64163447676ed8545b1e149bd848d32ed2334e0c
Yahir Keym <arzkaner@gmail.com>
2019-08-05 17:34:14 -0500

    Se modifica trae datos para poder hacer callback

--- src/components/TraeDatos.js
+++ src/components/TraeDatos.js
@@ -1,9 +1,15 @@
 // Es una función asyncrona que nos traera los datos de la url que le pidamos siempre y cuando su acción sea "View"
 // También los insertara en el lugar donde le digamos y por automatico nos pintara error de haberlo
 // y quitara el loading
-async function TraeDatos({url,_self},lugar = ""){
+async function TraeDatos({url,_self},lugar = "",action = "",callBack){
+    if(action === undefined){
+        action = "";
+    }
+    if(action.length === 0){
+        action = "view";
+    }
     try{
-        const response = await fetch(`${url}&action=view`);
+        const response = await fetch(`${url}&action=${action}`);
         const data = await response.json();
         if(data.datos === undefined){
             data.datos = [];
@@ -12,21 +18,26 @@ async function TraeDatos({url,_self},lugar = ""){
         {
             lugar = "data";
         }
-        _self.setState(
-            {
-                loading: false,
-                [lugar]: data.datos,
-                error: false
-            }
-        );
+        if(data.status){
+            _self.setState(
+                {
+                    loading: false,
+                    [lugar]: data.datos,
+                    error: false
+                }
+            );
+        }
+        if(callBack !== undefined){
+            callBack(_self,_self.state.data);
+        }
     }catch(error){
         _self.setState({
             error: true,
             loading:false
         })
-        _self.traeDatos = setTimeout(() => {
-            TraeDatos({'url':url,'_self':_self})
-        },5000)
+        // _self.traeDatos = setTimeout(() => {
+        //     TraeDatos({'url':url,'_self':_self})
+        // },5000)
     }
 }
 export default TraeDatos;
