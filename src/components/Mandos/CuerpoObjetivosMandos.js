571d28c013c9dd7ffcefcb91f6b998bf82e720b1
Yahir Keym <arzkaner@gmail.com>
2019-08-07 09:57:38 -0500

    Se configura el cuerpo para no mostrar descripciones en caso de que no haya

--- src/components/Mandos/CuerpoObjetivosMandos.js
+++ src/components/Mandos/CuerpoObjetivosMandos.js
@@ -2,36 +2,21 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import './styles/Mandos.css';
 import DeleteAction from '../DeleteAction.js';
-/**
- * Cambiara los acentos que vengan con codificación java
- */
-function acentosEncodeJava(cadena = ""){
-    cadena = cadena.replace(/u00e1/gi,"á");
-    cadena = cadena.replace(/u00C1/gi,"Á");
-    cadena = cadena.replace(/u00E9/gi,"é");
-    cadena = cadena.replace(/u00C9/gi,"É");
-    cadena = cadena.replace(/u00ED/gi,"í");
-    cadena = cadena.replace(/u00CD/gi,"Í");
-    cadena = cadena.replace(/u00f3/gi,"ó");
-    cadena = cadena.replace(/u00D3/gi,"Ó");
-    cadena = cadena.replace(/u00fa/gi,"ú");
-    cadena = cadena.replace(/u00DA/gi,"Ú");
-    cadena = cadena.replace(/u00F1/gi,"ñ");
-    cadena = cadena.replace(/u00D1/gi,"Ñ");
-    return cadena;
-}
+import DecodificaMalos from '../Generales/DecodificaMalos';
 /**
  * Guarda el componente de descrición del Mando
  * @param {propiedades} props 
  */
 function Descripcion(props){
-    let Descripcion = acentosEncodeJava(props.descripcion);
-    if(Descripcion.length === 0){
-        Descripcion = "Este indicador no cuenta con una descripción, por favor de agregar una";
+    let Descripcion = (
+        <div className="col-12 p-2 mando-text">
+            <p>{DecodificaMalos(props.descripcion)}</p>
+        </div>
+    );
+    if(props.descripcion.length === 0){
+        Descripcion = "";
     }
-    return(
-            <p>{Descripcion}</p>
-    )
+    return Descripcion
 }
 /**
  * Guarda el componente del titulo
@@ -39,9 +24,9 @@ function Descripcion(props){
  */
 function Titulo(props)
 {
-    let MuestraTitulo = acentosEncodeJava(props.titulo);
+    let MuestraTitulo = DecodificaMalos(props.titulo);
     if(MuestraTitulo.length === 0){
-        MuestraTitulo = acentosEncodeJava(props.objetivo)
+        MuestraTitulo = DecodificaMalos(props.objetivo)
     }
     return(
         <h4>{MuestraTitulo}</h4>
@@ -79,9 +64,7 @@ function CuerpoObjetivosMandos(props){
             <div  className="col-12 text-center">
                 <Titulo titulo={titulo} objetivo={subtitulo} />
             </div>
-            <div className="col-12 p-2 mando-text">
-                <Descripcion descripcion={descripcion} />
-            </div>
+            <Descripcion descripcion={descripcion} />
             {props.children}
             <div className="col-12 mt-3">
                 {isProfile && (<button className="btn btn-success" onClick={onClickSave}>Guardar</button>)}                
